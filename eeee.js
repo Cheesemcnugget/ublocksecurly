bfaa6d4c86bc07d678a253c594a993cd60b2b619
Cheesemcnugget <124630274+Cheesemcnugget@users.noreply.github.com>
2024-09-06 17:19:42 -0400

    Update eeee.js

--- eeee.js
+++ eeee.js
@@ -1,8 +1,1 @@
-window.addEventListener("keyup", event => {
-  if (event.ctrlKey && event.which === 192) { // 192 is the key code for backtick `
-      var newURL = prompt("Enter the URL to go to:");
-      if (newURL) {
-          window.location.href = newURL;
-      }
-  }
-});
+alert('Script loaded');
