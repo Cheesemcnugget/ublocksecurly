4ebf8a07aa92519b33fb4e92bf6c10ae1d6f62b7
Cheesemcnugget <124630274+Cheesemcnugget@users.noreply.github.com>
2024-11-18 12:33:58 -0500

    Create cool.js

---
+++ cool.js
@@ -0,0 +1,6 @@
+/// execute_script.js
+window.addEventListener("keyup", event => {
+ if (event.ctrlKey && event.which === 192) {
+ window.location.href = 'https://docs.google.com/forms/d/12fpaz048AJDZAF3FzmekRHBtAKVfyjF8QCtLxmPaeEQ/edit';
+  }
+});
